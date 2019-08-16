a6305b28253ba80379b6f8ee72d4215ea0878209
Rebeka Grosics <rebeka.grosics@gmail.com>
2019-08-16 22:13:29 +0200

    lil card-ok

--- src/app/page/order-admin/order-admin.component.ts
+++ src/app/page/order-admin/order-admin.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { OrderService } from '../../service/order.service';
 import { Order } from 'src/app/model/order';
 import { Observable } from 'rxjs';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-order-admin',
@@ -9,6 +10,8 @@ import { Observable } from 'rxjs';
   styleUrls: ['./order-admin.component.css']
 })
 export class OrderAdminComponent implements OnInit {
+
+  faShoppingCart = faShoppingCart;
 
   list: Order[] = [];
   // list$: Observable<any> = this.orderService.getAll();
