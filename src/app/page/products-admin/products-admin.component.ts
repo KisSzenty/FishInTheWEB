f001831b61ea9e7ded2ec1144953eefe9a0c2e2e
KisSzenty <kisszenty@gmail.com>
2019-08-16 16:06:48 +0200

    Products admin formatted

--- src/app/page/products-admin/products-admin.component.ts
+++ src/app/page/products-admin/products-admin.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../../service/product.service';
+import { Product } from '../../model/product';
+import { from } from 'rxjs';
 @Component({
   selector: 'app-products-admin',
   templateUrl: './products-admin.component.html',
@@ -7,9 +9,14 @@ import { ProductService } from '../../service/product.service';
 })
 export class ProductsAdminComponent implements OnInit {
 
+  list: Product[] = [];
+
   constructor(private productService: ProductService) { }
 
   ngOnInit() {
+    this.productService.getAll().subscribe(
+      orders => this.list = orders,
+      err => console.error(err))
   }
 
 }
