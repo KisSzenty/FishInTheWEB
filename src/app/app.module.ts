712beb0fdf03027a5bffb0165c27e200d6e15077
Rebeka Grosics <rebeka.grosics@gmail.com>
2019-08-16 15:04:27 +0200

    conflict

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,9 @@ import { IndexComponent } from './page/index/index.component';
 import { OrderAdminComponent } from './page/order-admin/order-admin.component';
 import { NavbarComponent } from './page/navbar/navbar.component';
 import { ProductsComponent } from './page/products/products.component';
-<<<<<<< HEAD
 import { OrderEditComponent } from './page/order-edit/order-edit.component';
 import { OrderAddComponent } from './page/order-add/order-add.component';
-=======
 import { OrderComponent } from './page/index/order/order.component';
->>>>>>> da5b881e5af7c6d768a81612f827d3c9bdcf7584
-
 @NgModule({
   declarations: [
     AppComponent,
@@ -23,12 +19,9 @@ import { OrderComponent } from './page/index/order/order.component';
     OrderAdminComponent,
     NavbarComponent,
     ProductsComponent,
-<<<<<<< HEAD
     OrderEditComponent,
-    OrderAddComponent
-=======
+    OrderAddComponent,
     OrderComponent
->>>>>>> da5b881e5af7c6d768a81612f827d3c9bdcf7584
   ],
   imports: [
     BrowserModule,
