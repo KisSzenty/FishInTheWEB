70848d75c9f6fb1b0314d9d630f04c25f02b35b8
Rebeka Grosics <rebeka.grosics@gmail.com>
2019-08-16 16:40:48 +0200

    pampampam

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,15 @@ import { IndexComponent } from './page/index/index.component';
 import { OrderAdminComponent } from './page/order-admin/order-admin.component';
 import { ProductsComponent } from './page/products/products.component';
 import { OrderComponent } from './page/order/order.component';
+import { OrderEditComponent } from './page/order-edit/order-edit.component';
+import { OrderAddComponent } from './page/order-add/order-add.component';
 
 
 const routes: Routes = [
   { path: '', component: IndexComponent },
   { path: 'admin/order', component: OrderAdminComponent },
+  { path: 'admin/order-edit', component: OrderEditComponent },
+  { path: 'admin/order-add', component: OrderAddComponent },
   { path: 'products', component: ProductsComponent },
   { path: 'order', component: OrderComponent },
   { path: '**', component: IndexComponent }
