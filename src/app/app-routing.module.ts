9a3a338751c8309982ad8f8dd4f26bb8c47a9a0c
KisSzenty <kisszenty@gmail.com>
2019-08-16 15:42:52 +0200

    Products component added to rooting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,17 @@ import { IndexComponent } from './page/index/index.component';
 import { OrderAdminComponent } from './page/order-admin/order-admin.component';
 import { ProductsComponent } from './page/products/products.component';
 import { OrderComponent } from './page/order/order.component';
+import { ProductsAdminComponent } from './page/products-admin/products-admin.component';
+import { ProductsEditComponent } from './page/products-edit/products-edit.component';
+import { ProductsAddComponent } from './page/products-add/products-add.component';
 
 
 const routes: Routes = [
   { path: '', component: IndexComponent },
   { path: 'admin/order', component: OrderAdminComponent },
+  { path: 'admin/products', component: ProductsAdminComponent },
+  { path: 'admin/products/add', component: ProductsAddComponent },
+  { path: 'admin/products/:id', component: ProductsEditComponent },
   { path: 'products', component: ProductsComponent },
   { path: 'order', component: OrderComponent },
   { path: '**', component: IndexComponent }
