5afd61b89f9a809d95fcfe644728b3ae4cbf2668
Pál Sára <pal.sara.ajandek@gmail.com>
2019-08-18 16:49:36 +0200

    order-info component added, product components changed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,6 +15,7 @@ import { CoralsComponent } from './products/categories/corals/corals.component';
 import { ToolsComponent } from './products/categories/tools/tools.component';
 import { AquariumsComponent } from './products/categories/aquariums/aquariums.component';
 import { Order } from './model/order';
+import { ProductInfoComponent } from './page/product-info/product-info.component';
 
 
 const routes: Routes = [
@@ -26,6 +27,7 @@ const routes: Routes = [
   { path: 'admin/products/add', component: ProductsAddComponent },
   { path: 'admin/products/:id', component: ProductsEditComponent },
   { path: 'products', component: ProductsComponent },
+  { path: 'product/:id', component: ProductInfoComponent },
   { path: 'order', component: OrderComponent },
   { path: 'order/:id', component: OrderComponent },
   { path: 'products/fishes', component: FishesComponent },
